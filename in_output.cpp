80f76880ea4277fc5823f1f8ac915f5741db3c9a
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-03 13:30:16 +1000

    Create in_output.cpp

---
+++ in_output.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string> //cstring for c string functions
+using namespace std;
+
+int main(){
+
+// define a string
+  
+string Cpp_string1 = 'hello!'; // string class
+
+//input and output
+int a,b,c;
+cin>>a>>b>>c;
+cout<<a+b+c<<endl
+
+//commom data type
+/*
+Int ("%d"): 32 Bit integer
+Long ("%ld"): 64 bit integer
+Char ("%c"): Character type
+Float ("%f"): 32 bit real value
+Double ("%lf"): 64 bit real value*/
+//example
+int a; long b; char c; float d; double e; // 3 12345678912345 a 334.23 14049.30493
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n\%c\n%f\n%.9lf", a, b, c, d,e);
+
+
+  
+}
