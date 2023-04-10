06d6819512147783c3d59b4a434b537375abf5e4
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-10 22:41:57 +1000

    Update Contractor.cpp

--- Classes/Contractor.cpp
+++ Classes/Contractor.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std
 
-# a constructor in C++ is a special method that is automatically called when an object of a class is created
+// a constructor in C++ is a special method that is automatically called when an object of a class is created
+// Constructors have the same name as the class, and have no return type
+// Constructor can write to const objects during their construction
+// default constructor(no parameters) is used if no initializer is supplied
 class Car {
   public:
   string brand;
   string model;
   int year;
-  Car(string x, string y, int z){
+  Car(string x, string y, int z){  
     brand = x;
     model = y;
     year = z;}
