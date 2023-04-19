1e027e3e7dc7f6b1fd1c3c717a98480baafbb73b
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-19 19:26:28 +1000

    Create inputFormat.cpp

---
+++ input_ouput_file/inputFormat.cpp
@@ -0,0 +1,13 @@
+//Controlling input format
+/*By default, the input operators ignore whitespace*/
+
+// 1-- noskipws causes th input operator to read, rather than skip whitespace
+/*a b c
+d*/
+while(cin>>ch)) cout<<ch; //abcd
+
+cin>>noskipws;
+while(cin>>ch)) cout<<ch;
+/*a b c
+d*/
+
