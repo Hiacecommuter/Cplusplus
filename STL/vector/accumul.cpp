19259219d7e0416b4e7e3c75253b96bc5b5c2623
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-21 21:23:03 +1000

    Create accumul.cpp

---
+++ STL/vector/accumul.cpp
@@ -0,0 +1,5 @@
+vector<int> arr;
+long long i;
+accumulate(arr.begin(), arr.end()-1, 0); // type of initail value deciding type of final value
+accumulate(arr.begin(), arr.end()-1, 0.0);
+accumulate(arr.begin(), arr.end()-1, i);
